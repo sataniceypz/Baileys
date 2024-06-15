8caafa322b9f8b5eb6a487771b7c9d3305cb1b8c
Adithyan <hermitx39@gmail.com>
2024-06-15 23:42:32 +0530

    newsletter

---
+++ lib/Types/Newsletter.d.ts
@@ -0,0 +1,19 @@
+export declare type RoleNewsLetter = 'SUBSCRIBER' | 'GUEST' | 'ADMIN' | 'OWNER';
+export declare type RectionSettingsNewsletter = 'ALL' | 'BASIC' | 'NONE' | 'BLOCKLIST';
+export interface NewsLetterMetadata {
+    id: string;
+    state: 'ACTIVE' | 'SUSPENDED' | 'GEOSUSPENDED';
+    creationTime: number;
+    inviteCode: string;
+    name: string;
+    desc: string;
+    subscriberCount: number;
+    verification: 'VERIFIED' | 'UNVERIFIED';
+    picture?: string;
+    preview?: string;
+    settings: {
+        reaction: RectionSettingsNewsletter;
+    };
+    mute?: 'ON' | 'OFF';
+    role?: RoleNewsLetter;
+}
